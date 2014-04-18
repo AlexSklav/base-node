33abebc9319d2eed4fe136baa8eddeebfe5afbf2
Christian Fobel <christian@fobel.net>
2014-04-18 07:26:14 -0400

    Add base_node header/sources discovery functions
    Add the following functions:
    
     - `base_node.get_sketch_directory`
     - `base_node.get_includes`
     - `base_node.get_sources`
    
    See corresponding docstrings in `base_node/__init__.py` for details.

--- base_node/__init__.py
+++ base_node/__init__.py
@@ -1,74 +1,55 @@
-from struct import pack, unpack
-
-# command codes
-CMD_GET_PROTOCOL_NAME       = 0x80
-CMD_GET_PROTOCOL_VERSION    = 0x81
-CMD_GET_DEVICE_NAME         = 0x82
-CMD_GET_MANUFACTURER        = 0x83
-CMD_GET_HARDWARE_VERSION    = 0x84
-CMD_GET_SOFTWARE_VERSION    = 0x85
-CMD_GET_URL                 = 0x86
-
-# reserved return codes
-RETURN_OK                   = 0x00
-RETURN_GENERAL_ERROR        = 0x01
-RETURN_UNKNOWN_COMMAND      = 0x02
-RETURN_TIMEOUT              = 0x03
-RETURN_NOT_CONNECTED        = 0x04
-RETURN_BAD_INDEX            = 0x05
-RETURN_BAD_PACKET_SIZE      = 0x06
-RETURN_BAD_CRC              = 0x07
-RETURN_BAD_VALUE            = 0x08
-RETURN_MAX_PAYLOAD_EXCEEDED = 0x09
+import os
+import glob
 
 
-class BaseNode():
-    def __init__(self, control_board, address):
-        self.control_board = control_board
-        self.address = address
-        self.write_buffer = []
+def get_sketch_directory():
+    '''
+    Return directory containing the `base_node` Arduino sketch.
+    '''
+    import base_node
+    return os.path.join(os.path.abspath(os.path.dirname(base_node.__file__)),
+                        'Arduino', 'BaseNode')
 
-    def protocol_name(self):
-        return self._get_string(CMD_GET_PROTOCOL_NAME)
 
-    def protocol_version(self):
-        return self._get_string(CMD_GET_PROTOCOL_VERSION)
+def get_includes():
+    '''
+    Return directories containing the `base_node` Arduino header files.
 
-    def name(self):
-        return self._get_string(CMD_GET_DEVICE_NAME)
+    Modules that need to compile against `base_node` should use this function
+    to locate the appropriate include directories.
 
-    def manufacturer(self):
-        return self._get_string(CMD_GET_MANUFACTURER)
+    Notes
+    =====
 
-    def hardware_version(self):
-        return self._get_string(CMD_GET_HARDWARE_VERSION)
+    For example:
 
-    def software_version(self):
-        return self._get_string(CMD_GET_SOFTWARE_VERSION)
+        import base_node
+        ...
+        print ' '.join(['-I%s' % i for i in cyvpr.get_includes()])
+        ...
 
-    def url(self):
-        return self._get_string(CMD_GET_URL)
+    '''
+    import base_node
+    return [get_sketch_directory()]
 
-    def send_command(self, cmd):
-        self.data = (self.control_board.i2c_send_command(self.address, cmd,
-                                                         self.write_buffer)
-                     .tolist())
-        self.write_buffer = []
 
-    def _get_string(self, cmd):
-        self.send_command(cmd)
-        return pack('B' * len(self.data), *self.data)
+def get_sources():
+    '''
+    Return `base_node` Arduino source file paths.
 
-    def read_float(self):
-        num = self.data[0:4]
-        self.data = self.data[4:]
-        return unpack('f', pack('BBBB', *num))[0]
+    Modules that need to compile against `base_node` should use this function
+    to locate the appropriate source files to compile.
 
-    def read_uint8(self):
-        return self.data.pop(0)
+    Notes
+    =====
 
-    def serialize_uint8(self, num):
-        self.write_buffer.append(num)
+    For example:
 
-    def serialize_float(self, num):
-        self.write_buffer.extend(unpack('BBBB', pack('f', num)))
+        import base_node
+        ...
+        print ' '.join(base_node.get_sources())
+        ...
+
+    '''
+    import base_node
+    return glob.glob(os.path.join(get_sketch_directory(), '*.c*'))
