29698be3ea40f41dd7bd7b3821165ba4246ccf2e
Christian Fobel <christian@fobel.net>
2018-02-20 00:31:32 -0500

    fix(conda): fix bld.bat script

--- .conda-recipe/bld.bat
+++ .conda-recipe/bld.bat
@@ -12,7 +12,7 @@ endlocal
 REM REM Build firmware
 pio run
 if errorlevel 1 exit 1
-# Copy Arduino library to Conda include directory
+REM Copy Arduino library to Conda include directory
 xcopy /S /Y /I /Q "%SRC_DIR%"\lib\%ARDUINO_NAME% "%PREFIX%"\Library\include\Arduino\%ARDUINO_NAME%
 REM REM Copy compiled firmware to Conda bin directory
 copy "%SRC_DIR%"\platformio.ini "%PREFIX%"\Library\bin\platformio\%PKG_NAME%
@@ -23,16 +23,6 @@ if errorlevel 1 exit 1
 
 REM Generate `setup.py` from `pavement.py` definition.
 "%PYTHON%" -m paver generate_setup
-if errorlevel 1 exit 1
-
-REM **Workaround** `conda build` runs a copy of `setup.py` named
-REM `conda-build-script.py` with the recipe directory as the only argument.
-REM This causes paver to fail, since the recipe directory is not a valid paver
-REM task name.
-REM
-REM We can work around this by wrapping the original contents of `setup.py` in
-REM an `if` block to only execute during package installation.
-"%PYTHON%" -c "from __future__ import print_function; input_ = open('setup.py', 'r'); data = input_.read(); input_.close(); output_ = open('setup.py', 'w'); output_.write('\n'.join(['import sys', 'import path_helpers as ph', '''if ph.path(sys.argv[0]).name == 'conda-build-script.py':''', '    sys.argv.pop()', 'else:', '\n'.join([('    ' + d) for d in data.splitlines()])])); output_.close(); print(open('setup.py', 'r').read())"
 if errorlevel 1 exit 1
 
 REM Install source directory as Python package.
